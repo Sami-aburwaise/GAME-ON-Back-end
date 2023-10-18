2c18f1da2c88de6de738a938cdfe5d6cc92bf98f
sami-aburwaise <tarrary1@gmail.com>
2023-10-18 14:28:11 +0300

    create coach model

---
+++ models/Coach.js
@@ -0,0 +1,17 @@
+const moongose = require('mongoose')
+
+const coachSchema = new moongose.Schema(
+  {
+    name: String,
+    description: String,
+    price: Number,
+    Games: [String]
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Coach = mongoose.model('Coach', coachSchema)
+
+module.exports = { Coach }
